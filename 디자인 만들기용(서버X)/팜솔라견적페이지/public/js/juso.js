5fc228d8c613caa29391e117b9bd6c835d281324
jeaHyeonSim <125427764+jeaHyeonSim@users.noreply.github.com>
2023-11-16 16:57:51 +0900

    2
    2

--- 디자인 만들기용(서버X)/팜솔라견적페이지/public/js/juso.js
+++ 디자인 만들기용(서버X)/팜솔라견적페이지/public/js/juso.js
@@ -18,7 +18,34 @@ $(document).on('click', '.result_li', function() {
     // addr_data = addr_list[$(this).index()]; // 선택한 주소 데이터
     addr_index = $(this).index();
 });
-// $('.modal-btn').trigger("click");
 
 // 모달창 실행 이벤트
 // $('.modal-btn').trigger("click");
+
+function eventClass() {
+    // 주소 리스트 생성될 경우 CSS 수정
+    if($('.addr_list').hasClass('on')) {
+        console.log($('.addr_list').hasClass('on'));
+        $('#formBg #fsearch .word-del').addClass("on");
+        $('#formBg').css({
+            'border-bottom-left-radius': 'unset',
+            'border-bottom-right-radius': 'unset'
+        });
+        $('.addr_list_inbox .addr_list').css({
+            'box-shadow':'0px 5px 15px -10px #000000'
+        });
+    }
+    if(!$('.addr_list').hasClass('on')) {
+        console.log($('.addr_list').hasClass('on'));
+        
+        $('#formBg #fsearch .word-del').removeClass("on");
+        $('#formBg').css({
+            'border-bottom-left-radius': '30px',
+            'border-bottom-right-radius': '30px'
+        });
+        $('.addr_list_inbox .addr_list').css({
+            'box-shadow':'none'
+        });
+    }
+}
+eventClass();
